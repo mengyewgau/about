f560f799a10103cb523f8a07aadb2a3da71eda30
Michael Gau <61722401+mygau91@users.noreply.github.com>
2023-11-15 11:59:43 +0800

    updates to portfolio

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import {CiLinkedin} from 'react-icons/ci'
-import {BsGithub} from 'react-icons/bs'
-
+import React from "react";
+import { CiLinkedin } from "react-icons/ci";
+import { BsGithub } from "react-icons/bs";
 
 const HeaderSocial = () => {
   return (
-    <div className='header_socials'>
-        <a href="https://www.linkedin.com/in/mengyewgau/" target="_blank"><CiLinkedin/></a>
-        <a href="https://www.github.com/mygau91" target="_blank"><BsGithub/></a>
+    <div className="header_socials">
+      <a href="https://www.linkedin.com/in/mengyewgau/" target="_blank">
+        <CiLinkedin />
+      </a>
+      <a href="https://www.github.com/mengyewgau" target="_blank">
+        <BsGithub />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocial
+export default HeaderSocial;
